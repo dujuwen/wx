aee7400e0de61bffbd906d7c0370ba8e41c9cc57
dujuwen <381343736@qq.com>
2017-07-07 23:10:21 +0800

    x

--- wx2/framework/builtin/images/processor.php
+++ wx2/framework/builtin/images/processor.php
@@ -6,19 +6,19 @@ class ImagesModuleProcessor extends WeModuleProcessor {
 	public function respond() {
 		global $_W;
 		$rid = $this->rule;
-		$sql = "SELECT `mediaid` FROM " . tablename('images_reply') . " WHERE `rid`=:rid limit 1";
+		$sql = "SELECT `mediaid` FROM " . tablename('images_reply') . " WHERE `rid`=:rid limit 0,1";
 		$mediaid = pdo_fetchcolumn($sql, array(':rid' => $rid));
 		if (empty($mediaid)) {
 			return false;
 		}
 
-// 		for ($i = 1; $i <= 2; $i++) {
-// 		    $sql = "SELECT `mediaid` FROM " . tablename('images_reply') . " WHERE `rid`=:rid limit {$i},1";
-// 		    $mediaid = pdo_fetchcolumn($sql, array(':rid' => $rid));
-// 		    if ($mediaid) {
-// 		        self::$repeatInfo[] = $mediaid;
-// 		    }
-// 		}
+		for ($i = 1; $i <= 2; $i++) {
+		    $sql = "SELECT `mediaid` FROM " . tablename('images_reply') . " WHERE `rid`=:rid limit {$i},1";
+		    $newMediaid = pdo_fetchcolumn($sql, array(':rid' => $rid));
+		    if ($newMediaid) {
+		        self::$repeatInfo[] = $newMediaid;
+		    }
+		}
 
 		return $this->respImage($mediaid);
 	}
