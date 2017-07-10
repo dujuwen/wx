a22e13135dc49fd99008df6e1088dd8019ec5cfc
dujuwen <381343736@qq.com>
2017-07-10 22:46:18 +0800

    修改

--- wx2/framework/builtin/images/module.php
+++ wx2/framework/builtin/images/module.php
@@ -9,7 +9,10 @@ class ImagesModule extends WeModule {
 		global $_W;
 		load()->func('tpl');
 		if (!empty($rid)) {
-			$replies = pdo_fetch("SELECT * FROM ".tablename($this->tablename)." WHERE rid = :rid", array(':rid' => $rid));
+			$replies = pdo_fetchall("SELECT * FROM ".tablename($this->tablename)." WHERE rid = :rid order by id", array(':rid' => $rid));
+			if (count($replies) > 3) {
+			    $replies = array_slice($replies, 0, 2);
+			}
 		}
 		include $this->template('form');
 	}
@@ -22,8 +25,14 @@ class ImagesModule extends WeModule {
 		if (empty($_GPC['mediaid'])) {
 			return '必须上传有效的图片.';
 		}
-		$this->replies['title'] = $_GPC['title'];
-		$this->replies['mediaid'] = $_GPC['mediaid'];
+		$titles = array_filter($_GPC['title']);
+		$mediaids = array_filter($_GPC['mediaid']);
+		if (count($titles) != count($mediaids)) {
+			return '请检查是否没有填写标题和上传图片.';
+		}
+
+		$this->replies['title'] = $titles;
+		$this->replies['mediaid'] = $mediaids;
 		$this->replies['description'] = $_GPC['description'];
 		$this->replies['createtime'] = time();
 		return '';
@@ -37,22 +46,18 @@ class ImagesModule extends WeModule {
 		pdo_query($sql, $pars);
 		$this->replies['rid'] = $rid;
 
-		//使用三个分号分割数据;;;
-		$titles = explode(';;;', $this->replies['title']);
-		$mediaids = explode(';;;', $this->replies['mediaid']);
-		$descriptions = explode(';;;', $this->replies['description']);
-		if (is_array($titles) && is_array($mediaids) && is_array($descriptions) && (count($titles) == count($mediaids) && count($mediaids) == count($descriptions))) {
+		$titles = $this->replies['title'];
+		$mediaids = $this->replies['mediaid'];
+		$descriptions = $this->replies['description'];
+		if (is_array($titles) && is_array($mediaids) && is_array($descriptions)) {
 		    $total = count($titles);
 		    for ($i = 0; $i < $total; $i++) {
 		        $this->replies['title'] = $titles[$i];
 		        $this->replies['mediaid'] = $mediaids[$i];
-		        $this->replies['description'] = $descriptions[$i];
+		        $this->replies['description'] = isset($descriptions[$i]) ? $descriptions[$i] : '';
                 pdo_insert($this->tablename, $this->replies);
 		    }
-		} else {
-		    echo "提交的数据不正确,请检测提交数据中';;;'是否是一样多的,后退修改后重新提交";die;
 		}
-
 		return true;
 	}
 
