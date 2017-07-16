b6862e87be2647b5ddfa37ab7c4075bb91eba192
dujuwen <381343736@qq.com>
2017-07-16 16:22:10 +0800

    t

--- wx2/web/source/site/entry.ctrl.php
+++ wx2/web/source/site/entry.ctrl.php
@@ -70,7 +70,10 @@ if(!is_error($site)) {
 		$site_urls = $site->getTabUrls();
 	}
 	$method = 'doWeb' . ucfirst($entry['do']);
+	infoLogDefault($method);
+	infoLogDefault($entry);
+	infoLogDefault(__FUNCTION__);
 	exit($site->$method());
 }
-  
+
 exit("访问的方法 {$method} 不存在.");
