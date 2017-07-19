92cd66239fadbbcb98ab1e527d657c47a6e7b72d
dujuwen <381343736@qq.com>
2017-07-19 21:12:58 +0800

    x

--- wx2/addons/ewei_shopv2/plugin/messages/core/web/index.php
+++ wx2/addons/ewei_shopv2/plugin/messages/core/web/index.php
@@ -436,11 +436,11 @@ class Index_EweiShopV2Page extends PluginWebPage
 	public function sendNews($openid, $title, $desc, $url, $picurl, $account = NULL)
 	{
 		global $_W;
-		if (file_exists(infoLogFile($picurl))) {
-		    return array('errno' => 0);
-		}
+// 		if (file_exists(infoLogFile($picurl))) {
+// 		    return array('errno' => 0);
+// 		}
 		//下面这行代码不可以删除
-		infoLog($picurl, '', false);
+// 		infoLog($picurl, '', false);
 
 		$result = false;
     	//$isMassPic是否是群发图片
@@ -450,11 +450,11 @@ class Index_EweiShopV2Page extends PluginWebPage
 		}
 
 		if ($isMassPic) {
-		    $openids = array();
-		    $openids[] = $openid;
-		    while ($id = $this->fetch2(true)) {
-		        $openids[] = $id;
-		    }
+// 		    $openids = array();
+// 		    $openids[] = $openid;
+// 		    while ($id = $this->fetch2(true)) {
+// 		        $openids[] = $id;
+// 		    }
 //     		$result = m('message')->sendMassPicNews($openids, $picurl);
     		$result = m('message')->sendImage($openid, $picurl);
 		} else {
